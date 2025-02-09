413b1516642545457721be5c2bb6e8dd732bffb6
setoudie <65713703+setoudie@users.noreply.github.com>
2025-02-09 14:38:09 +0000

    Update generate_time.py
    append a new line every run

--- generate_time.py
+++ generate_time.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 # Génère un fichier avec la date et l'heure actuelles
 current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-with open("current_time.txt", "w") as file:
+with open("current_time.txt", "a") as file:
     file.write(f"La date et l'heure actuelles sont : {current_time}\n")
 
 print("Fichier current_time.txt mis à jour avec succès !")
